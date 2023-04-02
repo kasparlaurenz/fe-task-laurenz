bcefa014aa82d119043104f0e7b1a76c79aa352b
Laurenz <laurenz.seipel@gmail.com>
2023-04-02 17:27:04 +0200

    Refactored

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import { FormData, User } from './types';
 
-const API_GET = 'https://randomuser.me/api/?inc=name,email';
+const API_GET = 'https://randomuser.me/api/';
 const API_POST = 'https://acc-test-vjn7.onrender.com/form';
 
 export const getRandomUser = async (): Promise<User> => {
-    const response = await axios.get(API_GET);
+    const response = await axios.get(`${API_GET}?inc=name,email`);
     const data = response.data.results[0];
     return data;
 };
@@ -13,7 +13,7 @@ export const getRandomUser = async (): Promise<User> => {
 export const postUser = async (formData: FormData): Promise<void> => {
     const headers = {
         'Content-Type': 'application/json',
-        'x-api-key': 'letmein',
+        'x-api-key': process.env.REACT_APP_API_KEY,
     };
     await axios.post(API_POST, formData, { headers });
 };
